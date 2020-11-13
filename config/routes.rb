1d1aa1d24286b1edb0c9ef55ba0085a524c33f8b
alexjeman <alexandrujeman.ja@gmail.com>
2020-11-13 10:36:32 +0200

    Add registration pec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: %w[show]
+  end
+
+  devise_for :users,
+             defaults: {format: :json},
+             path: '',
+             path_names: {
+               sign_in: 'users/login',
+               sign_out: 'users/logout',
+               registration: 'users/signup'
+             },
+             controllers: {
+               sessions: 'session',
+               registrations: 'registrations'
+             }
 end
